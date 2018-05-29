6c7e2f6ccee1da97107e67e8a8cabffaaddab66c
Puczka <kjppster@gmail.com>
2018-05-29 21:13:29 +0200

    ADD: Test Http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NgFullLoaderService} from '../../projects/ng-full-loader/src/lib/ng-full-loader.service';
 import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,19 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private loaderService: NgFullLoaderService, private router: Router) {
+  constructor(private loaderService: NgFullLoaderService, private router: Router, private httpClient: HttpClient) {
   }
 
   public showLoader() {
     this.loaderService.showLoader(0);
     setTimeout(() => this.loaderService.hideLoader(), 5000);
   }
-  public testRouterLoader(){
+
+  public testHttp() {
+    this.httpClient.get('http://www.mocky.io/v2/5b0da594310000b20c9d5793?mocky-delay=5000ms').subscribe();
+  }
+
+  public testRouterLoader() {
     this.router.navigate(['routerTest']);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {NgFullLoaderModule} from '../../projects/ng-full-loader/src/lib/ng-full-loader.module';
-import {RouterModule, Routes} from '@angular/router';
-import {RouterLoaderService} from './router-loader.service';
 import { RouterLoaderComponent } from './router-loader/router-loader.component';
 import {AppRoutingModule} from './app-routing.module';
-import {MatSlideToggleModule} from '@angular/material';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -20,9 +18,11 @@ import {MatSlideToggleModule} from '@angular/material';
       enableForRouting: true
       },
     ),
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
+
   ],
   bootstrap: [AppComponent]
 })
